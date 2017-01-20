bde2bbb60850b054e36ac8ad27cf16962c274207
pratik14 <pratik14shah@gmail.com>
2017-01-20 13:57:43 +0530

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    'webpack-hot-middleware/client',
+    './app/index.js'
+  ],
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+};
